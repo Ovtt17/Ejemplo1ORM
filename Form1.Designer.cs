5296263e59e2ea8a4a79d72cbce345aac1280c6f
Ovett Mora <101598433+Ovtt17@users.noreply.github.com>
2024-03-25 17:48:16 -0600

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Ejemplo1ORM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnGuardar = new DevExpress.XtraEditors.SimpleButton();
+            this.unitOfWork1 = new DevExpress.Xpo.UnitOfWork(this.components);
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.xpProductos = new DevExpress.Xpo.XPCollection(this.components);
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.colidproducto = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colnombreprod = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colfecha_vence = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colprecio = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colestante = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.BtnEliminar = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xpProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGuardar.Appearance.Options.UseFont = true;
+            this.BtnGuardar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnGuardar.ImageOptions.Image")));
+            this.BtnGuardar.Location = new System.Drawing.Point(809, 113);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(141, 52);
+            this.BtnGuardar.TabIndex = 0;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.xpProductos;
+            this.gridControl1.Location = new System.Drawing.Point(12, 113);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(754, 348);
+            this.gridControl1.TabIndex = 1;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // xpProductos
+            // 
+            this.xpProductos.ObjectType = typeof(Ejemplo1ORM.bdventa.Producto);
+            this.xpProductos.Session = this.unitOfWork1;
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colidproducto,
+            this.colnombreprod,
+            this.colfecha_vence,
+            this.colprecio,
+            this.colestante,
+            this.gridColumn1,
+            this.gridColumn2});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // colidproducto
+            // 
+            this.colidproducto.FieldName = "idproducto";
+            this.colidproducto.Name = "colidproducto";
+            this.colidproducto.Visible = true;
+            this.colidproducto.VisibleIndex = 0;
+            // 
+            // colnombreprod
+            // 
+            this.colnombreprod.FieldName = "nombreprod";
+            this.colnombreprod.Name = "colnombreprod";
+            this.colnombreprod.Visible = true;
+            this.colnombreprod.VisibleIndex = 1;
+            // 
+            // colfecha_vence
+            // 
+            this.colfecha_vence.FieldName = "fecha_vence";
+            this.colfecha_vence.Name = "colfecha_vence";
+            this.colfecha_vence.Visible = true;
+            this.colfecha_vence.VisibleIndex = 2;
+            // 
+            // colprecio
+            // 
+            this.colprecio.FieldName = "precio";
+            this.colprecio.Name = "colprecio";
+            this.colprecio.Visible = true;
+            this.colprecio.VisibleIndex = 3;
+            // 
+            // colestante
+            // 
+            this.colestante.FieldName = "estante";
+            this.colestante.Name = "colestante";
+            this.colestante.Visible = true;
+            this.colestante.VisibleIndex = 4;
+            // 
+            // gridColumn1
+            // 
+            this.gridColumn1.FieldName = "categoria_idcategoria!";
+            this.gridColumn1.Name = "gridColumn1";
+            // 
+            // gridColumn2
+            // 
+            this.gridColumn2.FieldName = "categoria_idcategoria!Key";
+            this.gridColumn2.Name = "gridColumn2";
+            this.gridColumn2.Visible = true;
+            this.gridColumn2.VisibleIndex = 5;
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnEliminar.Appearance.Options.UseFont = true;
+            this.BtnEliminar.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.BtnEliminar.Location = new System.Drawing.Point(809, 200);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(141, 52);
+            this.BtnEliminar.TabIndex = 2;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 549);
+            this.Controls.Add(this.BtnEliminar);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.BtnGuardar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.unitOfWork1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xpProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.SimpleButton BtnGuardar;
+        private DevExpress.Xpo.UnitOfWork unitOfWork1;
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.Xpo.XPCollection xpProductos;
+        private DevExpress.XtraGrid.Columns.GridColumn colidproducto;
+        private DevExpress.XtraGrid.Columns.GridColumn colnombreprod;
+        private DevExpress.XtraGrid.Columns.GridColumn colfecha_vence;
+        private DevExpress.XtraGrid.Columns.GridColumn colprecio;
+        private DevExpress.XtraGrid.Columns.GridColumn colestante;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn1;
+        private DevExpress.XtraGrid.Columns.GridColumn gridColumn2;
+        private DevExpress.XtraEditors.SimpleButton BtnEliminar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using Ejemplo1ORM.bdventa;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejemplo1ORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            Producto p = new Producto(unitOfWork1);
+            p.nombreprod = "pantalla";
+            p.estante = 10;
+            p.fecha_vence = DateTime.Today;
+            p.precio = 100;
+
+            Categoria categoria = new Categoria(unitOfWork1);
+            categoria.idcategoria = 5;
+            p.categoria_idcategoria = categoria;
+
+            p.Save();
+            unitOfWork1.CommitChanges();
+            xpProductos.Reload();
+        }
+
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            Producto producto = (Producto) gridView1.GetFocusedRow();
+            DialogResult d = MessageBox.Show("Estas seguro de eliminar este producto: ", "Eliminando Producto", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (d == DialogResult.OK)
+            {
+                unitOfWork1.Delete(producto);
+                unitOfWork1.CommitChanges();
+                xpProductos.Reload();
+            }
+        }
+    }
+}
